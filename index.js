210a72a854858b15ec26dcac4ebb9906624fb9a2
Moringa School <dorcasmosonik@gmail.com>
2020-08-23 17:07:39 +0300

    styled the output element

--- index.js
+++ index.js
@@ -20,52 +20,55 @@ function dayOfBirth() {
     switch (gender_value) {
         case "female":
             if (dayOfBirth == 0) {
-                document.getElementById("display").innerHTML= "You Were you born on" + day[0] + "and your Akhan name is" + femaleNames[0];
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[0] + "</span> and your Akhan name is <span>" + femaleNames[0];
             }
             if (dayOfBirth == 1) {
-                alert("You Were you born on" + day[1] + "and your Akhan name is" + femaleNames[1]);
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[1] + "</span> and your Akhan name is <span>" + femaleNames[1];
             }
             if (dayOfBirth == 2) {
-                alert("You Were you born on" + day[2] + "and your Akhan name is" + femaleNames[2]);
-            }
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[2] + "</span> and your Akhan name is <span>" + femaleNames[2];
+            }                     
             if (dayOfBirth == 3) {
-                alert("You Were you born on" + day[3] + "and your Akhan name is" + femaleNames[3]);
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[3] + "</span> and your Akhan name is <span>" + femaleNames[3];
             }
             if (dayOfBirth == 4) {
-                alert("You Were you born on" + day[4] + "and your Akhan name is" + femaleNames[4]);
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[4] + "</span> and your Akhan name is <span>" + femaleNames[4];
             }
             if (dayOfBirth == 5) {
-                alert("You Were you born on" + day[5] + "and your Akhan name is" + femaleNames[5]);
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[5] + "</span> and your Akhan name is <span>" + femaleNames[5];
             }
             if (dayOfBirth == 6) {
-                alert("You Were you born on" + day[6] + "and your Akhan name is" + femaleNames[6]);
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[6] + "</span> and your Akhan name is <span>" + femaleNames[6];
             }
             break;
         case "male":
             if (dayOfBirth == 0) {
-                alert("You Were you born on" + day[0] + "and your Akhan name is" + maleNames[0]);
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[0] + "</span> and your Akhan name is <span>" + maleNames[0];
             }
             if (dayOfBirth == 1) {
-                alert("You Were you born on" + day[1] + "and your Akhan name is" + maleNames[1]);
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[1] + "</span> and your Akhan name is <span>" + maleNames[1];
             }
             if (dayOfBirth == 2) {
-                alert("You Were you born on" + day[2] + "and your Akhan name is" + maleNames[2]);
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[2] + "</span> and your Akhan name is <span>" + maleNames[2];
             }
             if (dayOfBirth == 3) {
-                alert("You Were you born on" + day[3] + "and your Akhan name is" + maleNames[3]);
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[3] + "</span> and your Akhan name is <span>" + maleNames[4];
             }
             if (dayOfBirth == 4) {
-                alert("You Were you born on" + day[4] + "and your Akhan name is" + maleNames[4]);
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[4] + "</span> and your Akhan name is <span>" + maleNames[5];
             }
             if (dayOfBirth == 5) {
-                alert("You Were you born on" + day[5] + "and your Akhan name is" + maleNames[5]);
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[5] + "</span> and your Akhan name is <span>" + maleNames[5];
             }
             if (dayOfBirth == 6) {
-                alert("You Were you born on" + day[6] + "and your Akhan name is" + maleNames[6]);
+                document.getElementById("display").innerHTML = "You Were you born on <span>" + day[6] + "</span> and your Akhan name is <span>" + maleNames[6];
             }
             break;
         default:
     }
-
+    //prevent refresh on form submit
+    var form = document.getElementById("myForm");
+    function handleForm(event) { event.preventDefault(); }
+    form.addEventListener('submit', handleForm);
 
 }
